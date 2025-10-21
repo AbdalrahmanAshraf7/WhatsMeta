6b8d43e0e4cf6284602c2f4bcf95c7ce7d3224f6
Abdalrahman15 <abdalrahman200324@gmail.com>
2025-10-21 10:19:22 +0300

    update

--- src/comp/Layout/Layout.jsx
+++ src/comp/Layout/Layout.jsx
@@ -88,18 +88,24 @@ export default function Layout() {
             <div className='flex ms-3 absolute bottom-8 left-[20px] gap-1 items-center '>
 
               <input placeholder='What do u need ? 😉' onChange={(e)=>setInputWhats(e.target.value)} value={inputWhats} type="text" className='bg-white rounded-full focus:outline-0 py-2 px-3 text-[13px] w-[210px]' />
-              <button
-                onClick={() => {
-                const phone = "201505753059";
-                const message = inputWhats.trim() || "Details, please";
-                const isMobile = /Android|iPhone|iPad/i.test(navigator.userAgent);
-                const baseUrl = isMobile ? "https://api.whatsapp.com/send":"https://web.whatsapp.com/send";
-                const finalUrl = `${baseUrl}?phone=${phone}&text=${encodeURIComponent(message)}`;
-                window.open(finalUrl, "_blank");}}
-                className='bg-gray-300 cursor-pointer w-[40px] h-[40px] rounded-full flex justify-center items-center'
-                >
-                <i className="fa-solid fa-paper-plane text-gray-50"></i>
-              </button>
+               <button
+  onClick={() => {
+    const phone = "201505753059";
+    const message = inputWhats.trim() || "Details, please";
+    const isMobile = /Android|iPhone|iPad/i.test(navigator.userAgent);
+    const baseUrl = isMobile ? "https://api.whatsapp.com/send" : "https://web.whatsapp.com/send";
+    const finalUrl = `${baseUrl}?phone=${phone}&text=${encodeURIComponent(message)}`;
+    
+    if (isMobile) {
+      window.location.href = finalUrl; // ✅ الحل هنا
+    } else {
+      window.open(finalUrl, "_blank");
+    }
+  }}
+  className="bg-gray-300 cursor-pointer w-[40px] h-[40px] rounded-full flex justify-center items-center"
+>
+  <i className="fa-solid fa-paper-plane text-gray-50"></i>
+</button>
 
            
             </div>
@@ -119,3 +125,13 @@ export default function Layout() {
   
   </>
 }
+
+
+
+
+
+
+
+
+
+ 
